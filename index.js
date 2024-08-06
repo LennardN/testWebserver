df51eb329db9ef3bb6cd1b033828df9e4fedca9e
LennardN <lennard.norden.ln@gmail.com>
2024-08-06 11:16:19 +0200

    added mysql imports

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
-var mysql      = require('mysql');
+var mysql = require('mysql')
+
 var connection = mysql.createConnection({
   host     : 'mysqltest:33060',
   user     : 'root',
