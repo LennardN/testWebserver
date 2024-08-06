196783251da57ca70077013f70f500a216776f58
LennardN <lennard.norden.ln@gmail.com>
2024-08-06 13:11:57 +0200

    .

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ var connection = mysql.createConnection({
 
 const PORT = 8080
 const app = express()
+try{
 connection.connect();
+}catch(e){
+    console.log("couldnt connect")
+}
 
 app.use(express.static(path.join(__dirname, 'client')))
 
