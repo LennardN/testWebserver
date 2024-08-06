f9375122a9f96120861f30ccf3ee5be934285674
LennardN <lennard.norden.ln@gmail.com>
2024-08-06 13:43:12 +0200

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-  host     : 'mysqltest',
+  host: '172.18.0.3:3306',
   user: 'root',
   password : '123',
 });
